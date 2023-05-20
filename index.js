cecf4f23ad22abc11a914f08d113ef13e81e70b0
AmanatUMahmud <amanatmahmud@gmail.com>
2023-05-20 19:32:04 +0600

    booking post api implemented

--- index.js
+++ index.js
@@ -96,6 +96,24 @@ async function run() {
       const bookings = await bookingsCollection.find(query).toArray();
       res.send(bookings);
     });
+    app.post("/bookings", verifyJWT, async (req, res) => {
+      const booking = req.body;
+      const query = {
+        appointmentDate: booking.appointmentDate,
+        email: booking.email,
+        treatment: booking.treatment,
+      };
+ 
+      const alreadyBooked = await bookingsCollection.find(query).toArray();
+ 
+      if (alreadyBooked.length) {
+        const message = `You already have a booking on ${booking.appointmentDate}`;
+        return res.send({ acknowledged: false, message });
+      }
+ 
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    });
   } finally {
   }
 }
