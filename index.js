6d1479532ebe9b4d0e3352d33697c44909e8c5d8
Turjo99 <imturjo27@gmail.com>
2023-05-20 19:20:38 +0600

    connected with mongo and stripe

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 const app = express();
+const stripe = require("stripe")(
+  "sk_test_51M6PqYC7ChjMGzPD6RXkbkipsIoYs9pCgphgT2jA1D5fPLi6DAQUKunKxuV1WK9DlTAHEfzv0eWuano5HTh2UV4L005FWjSV5x"
+);
+
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xyxb77f.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  console.log(authHeader);
+  if (!authHeader) {
+    return res.status(401).send("unauthorized access");
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 app.get("/", (req, res) => {
   res.send("Simple Server Running");
 });
