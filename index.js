a83c87219364eaee0ccb74a98dfb690e530be3c6
Turjo99 <imturjo27@gmail.com>
2023-05-20 19:40:17 +0600

    json web token created for security

--- index.js
+++ index.js
@@ -153,6 +153,18 @@ async function run() {
       );
       res.send(result);
     });
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "10d",
+        });
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: "" });
+    });
   } finally {
   }
 }
