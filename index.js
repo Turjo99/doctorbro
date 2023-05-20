041b7d048ded14e7153c2aad3e6afcf790d657ea
AmanatUMahmud <amanatmahmud@gmail.com>
2023-05-20 19:46:05 +0600

    user updated

--- index.js
+++ index.js
@@ -170,6 +170,12 @@ async function run() {
       const users = await usersCollection.find(query).toArray();
       res.send(users);
     });
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+ 
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
   } finally {
   }
 }
