f15bca52ece66769b1f6c090ef9168edf12a229a
AmanatUMahmud <amanatmahmud@gmail.com>
2023-05-20 19:30:51 +0600

    admin verification implemented

--- index.js
+++ index.js
@@ -74,7 +74,16 @@ async function run() {
       });
       res.send(options);
     });
-    
+    const verifyAdmin = async (req, res, next) => {
+      const decodedEmail = req.decoded.email;
+      const query = { email: decodedEmail };
+      const user = await usersCollection.findOne(query);
+ 
+      if (user?.role !== "admin") {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
     
   } finally {
   }
